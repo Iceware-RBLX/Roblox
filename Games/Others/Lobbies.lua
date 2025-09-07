b755d3e28e0eae8c00b8822f5322fa8de156a3db
xander <89797002+nxtcapi@users.noreply.github.com>
2025-09-07 18:16:58 +0200

    Create Lobbies.lua

---
+++ Games/Others/Lobbies.lua
@@ -0,0 +1,5 @@
+game.StarterGui:SetCore("SendNotification", {
+	Title = "IceWare 🧊";
+	Text = "You are currently in the lobby of a supported game, Please join a match before executing.";
+	Duration = "10";
+})
