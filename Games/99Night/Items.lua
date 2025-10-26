3770de917542922176c6cc3402fdbe6246d6df8e
xander <89797002+nxtcapi@users.noreply.github.com>
2025-10-26 14:51:42 +0100

    Create Items.lua

---
+++ Games/99Night/Items.lua
@@ -0,0 +1,57 @@
+return {
+    raw_food = {
+        "Morsel",
+        "Steak",
+    };
+    cooked_food = {
+        "Cooked Morsel",
+        "Cooked Steak",
+    };
+    food = {
+        "Carrot",
+        "Pumpkin",
+        "Berry",
+        "Apple"
+        "Cake", 
+    }
+    weapon = {
+        "Revolver"
+        "Rifle",
+    };
+    fuel = {
+        "Coal",
+        "Log",
+        "Sapling",
+        "Chair",
+        "Fuel Canister"
+
+        "Bear Corpse",
+        "Wolf Corpse"
+    };
+    scrap = {
+        "Sheet Metal",
+        "Bolt",
+        "Broken Microwave",
+        "Tyre",
+        "UFO Scrap",
+        "Cultist Gem",
+    };
+    ammo = {
+        "Revolver Ammo",
+        "Rifle Ammo",
+    };
+    armour = {
+        "Leather Body",
+    };
+    tools = {
+        "Old Flashlight",
+        "Old Rod",
+        "Old Taming Flute",
+        "Good Axe",
+        "Good Sack"
+    };
+    heal = {
+        "Bandage",
+        "MedKit",
+    }
+}
