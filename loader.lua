5e8488c4d7a1f56a85a094cda80e85ac799adb41
xander <89797002+nxtcapi@users.noreply.github.com>
2025-10-30 15:09:43 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 --[[
-    Hi 😉
+   
 ]]
 
 
@@ -8,10 +8,6 @@ if game:GetService('ContentProvider').RequestQueueSize > 10 then
     until game:GetService('ContentProvider').RequestQueueSize <= 10
 end
 
--- if identifyexecutor():lower():find("xeno") or identifyexecutor():lower():find("solara") then --dont need as of now will do later tho
---     game.Players.LocalPlayer:Kick("[  ICEWARE  ] Executor not supported; Please use another executor such as volcano")
---     return;
--- end 
 
 local shared = {
     version = "V3.0.0";
@@ -24,8 +20,8 @@ local shared = {
 
 for _, folder in pairs(shared.folders) do
     if not isfolder(folder) then
-        print("[  ICEWARE  ]  Setting up "..folder.." folder.")
-        makefolder(folder)          
+        print(string.format("<font color='rgb(150,220,255)' size='18'>[ ICEWARE ]  Setting up %s folder.</font>", folder))
+        makefolder(folder)
     end
 end
 
@@ -35,6 +31,9 @@ local games = {
     [{13864661000}] = "https://raw.githubusercontent.com/Iceware-RBLX/Roblox/refs/heads/main/Games/Others/Lobbies.lua"; -- Break In 2 (Lobby)
     [{13864667823}] = "https://raw.githubusercontent.com/Iceware-RBLX/Roblox/refs/heads/main/Games/Break%20In%202/Main.lua"; -- Break In 2 (Main Match)
     [{93978595733734}] = "https://raw.githubusercontent.com/Iceware-RBLX/Roblox/refs/heads/main/Games/ViolenceDistrict/Main.lua"; -- Violence District
+    [{79546208627805}] = "https://raw.githubusercontent.com/Iceware-RBLX/Roblox/refs/heads/main/Games/Others/Lobbies.lua"; -- 99 Nights (Lobby)
+    [{126509999114328}] = "https://raw.githubusercontent.com/Iceware-RBLX/Roblox/refs/heads/main/Games/Others/Lobbies.lua"; -- 99 Nights (Main Match)
+
 }
 
 for ids, url in next, games do
@@ -49,3 +48,4 @@ if getgenv().Settings and getgenv().Settings.Analytics then
         appendfile("IceWare/analytics/" .. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name .. "_" .. os.date("%Y-%m-%d") .. ".log", "[" .. os.date("%H:%M:%S") .. "] [" .. identifyexecutor() .. "] " .. select(1, ...) .. "\n") 
     end)
 end
+
