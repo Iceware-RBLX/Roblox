7c1b0f09f7d0e9b6317e14bb4982fd1a71a59615
xander <89797002+nxtcapi@users.noreply.github.com>
2025-11-03 12:44:43 +0100

    Update Lobbies.lua

--- Games/Others/Lobbies.lua
+++ Games/Others/Lobbies.lua
@@ -1,3 +1,5 @@
+-- Yea i need to change the notif if i remember it
+
 game.StarterGui:SetCore("SendNotification", {
 	Title = "IceWare 🧊";
 	Text = "You are currently in the lobby of a supported game, Please join a match before executing.";
