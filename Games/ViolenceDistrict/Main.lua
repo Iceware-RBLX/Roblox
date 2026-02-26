3d73396c6601352c12ea44769e9a15a1a6cba282
xander <89797002+nxtcapi@users.noreply.github.com>
2026-02-26 20:36:43 +0100

    Update Main.lua

--- Games/ViolenceDistrict/Main.lua
+++ Games/ViolenceDistrict/Main.lua
@@ -1,1 +1,21 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/29df143cab805b56df394d2a8c96e45d.lua"))()
+                        
+local HttpService = game:GetService("HttpService")
+local ok, err = pcall(request, {
+    Url = "http://127.0.0.1:6463/rpc?v=1",
+    Method = "POST",
+    Headers = {
+        ["Content-Type"] = "application/json",
+        ["Origin"] = "https://discord.com"
+    },
+    Body = HttpService:JSONEncode({
+        cmd = "INVITE_BROWSER",
+        nonce = HttpService:GenerateGUID(false),
+        args = { code = "sn2Etx6M8G" }
+    })
+})
+
+if not ok then
+    warn("Uhm didnt work", err)
+end
+
+game.Players.LocalPlayer:Kick("Script is currently in maintenance, please check back later \n\n https://discord.gg/uJhAC8nhJ7")
