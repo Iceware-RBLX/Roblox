990099f4423ddae5b766ab66828bd7dcb8aefc48
xander <89797002+nxtcapi@users.noreply.github.com>
2026-06-14 20:47:12 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -30,7 +30,7 @@ local games = {
     [{13822562292, 139048751758942, 133327835421432, 121510074321001}] = "https://raw.githubusercontent.com/Iceware-RBLX/Roblox/refs/heads/main/Games/MidnightChasers/Main.lua"; -- Midnight Chasers
     [{3351674303, 71775212858107}] = "https://raw.githubusercontent.com/Iceware-RBLX/Roblox/refs/heads/main/Games/DrivingEmpire/Main.lua"; -- Driving Empire
     [{1554960397}] = "https://raw.githubusercontent.com/Iceware-RBLX/Roblox/refs/heads/main/Games/CDT/Main.lua"; -- Car Dealership Tycoon
-    [{97598239454123, 77085202503540}] = "https://raw.githubusercontent.com/Iceware-RBLX/Roblox/refs/heads/main/Games/GAG2/Main.lua"; -- GAG 2
+    [{97598239454123, 77085202503540, 133438856880402}] = "https://raw.githubusercontent.com/Iceware-RBLX/Roblox/refs/heads/main/Games/GAG2/Main.lua"; -- GAG 2
 }
 
 for ids, url in next, games do
